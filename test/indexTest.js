235fbf499dc2e1edbb9af1e4ba451ccfc86423c5
Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-05-08 12:30:10 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-describe('objects', function() {
+ describe('objects', function() {
   describe('Scooter constructor', function(){
     it("should create a new scooter with three properties", function(){
       let samson = new Scooter(2015, "red", "swoosh");
